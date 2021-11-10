6d8f2cfa53bccacb3dadffb760bf39690ff45a23
alexsoyes <contact.alexsoyes@gmail.com>
2021-11-10 09:39:16 +0100

    Exo 2 : Navigation

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
