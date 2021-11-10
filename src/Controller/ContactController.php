454eb61a53d9f557dfdf6fdbda9ba48b0e321b9b
juxqq <enzo.tremblay@epsi.fr>
2021-11-10 10:21:16 +0100

    Exo 2

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,16 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact", name="contact")
-     */
-    public function index(): Response
-    {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-        ]);
-    }
-    /**
-     * @Route("/contact/{type}", name="contact_type")
+     * @Route("/contact/{type}", name="contact")
      */
     public function types(Request $request, string $type = ""): Response
     {
