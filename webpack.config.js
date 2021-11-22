aeb58988b579ad6f1abec66cda85d84de191889d
alexsoyes <contact.alexsoyes@gmail.com>
2021-11-22 15:06:19 +0100

    Exo 5 : Ajout SaSS

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    // .addEntry('app', './assets/app.js')
     .addStyleEntry('contact', './assets/styles/contact.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
