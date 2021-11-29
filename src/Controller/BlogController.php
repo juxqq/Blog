9e1f60b1f94dd8d9b00883bdae22c01e001fc24d
alexsoyes <contact.alexsoyes@gmail.com>
2021-11-29 15:52:55 +0100

    Utilisation des sessions

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
@@ -11,8 +12,10 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index(SessionInterface $session): Response
     {
+        dump($session->get('name'));
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
         ]);
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -11,8 +12,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(SessionInterface $session): Response
     {
+        $session->set('name', 'alex');
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
